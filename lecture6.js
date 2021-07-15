70c3a2ed4dde74dde27bac3186049c7bd81563cc
Sarosh <saroshabdullah2013@gmail.com>
2021-07-16 03:10:33 +0530

    Destructuring

---
+++ lecture6.js
@@ -0,0 +1,12 @@
+// Destructuring
+
+const numbers = [1, 2, 3];
+
+[num1, num2] = numbers;
+
+console.log(num1, num2);
+
+console.log(numbers);
+
+// this destructing method is used to extract the elements from an array to the variables
+// or extracting the properties from objects to the variables.
