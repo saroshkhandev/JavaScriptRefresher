c6e5e2d565167209a750104c534c2b1e2997591d
Sarosh <saroshabdullah2013@gmail.com>
2021-08-02 16:31:01 +0530

    Async functions

--- lecture9.js
+++ lecture9.js
@@ -1,16 +1,26 @@
 // Async functions
 
-const fetchData = (callback) => {
-  setTimeout(() => {
-    callback("Done!");
-  }, 1500);
+// the callback function will be excecuted when the inner function is completed that is the timer function
+// this callback function is called inside the inner function.
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, 1500);
+  });
+  return promise;
 };
 
+// we call our fetchData function inside this timeout function then out callback function is called with
+// those values
 setTimeout(() => {
   console.log("Time is done!");
-  fetchData((text) => {
-    console.log(text);
-  });
+  fetchData()
+    .then((text) => {
+      console.log(text);
+      return fetchData();
+    })
+    .then((text2) => console.log(text2));
 }, 2000);
 
 console.log("Hello");
